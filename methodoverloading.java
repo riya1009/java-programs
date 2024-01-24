580aef4de31c5e9ba46588da735281145e9dfb63
RIYA BANSAL <srbiiyam@gmail.com>
2024-01-24 08:26:44 +0530

    Add files via upload

---
+++ methodoverloading.java
@@ -0,0 +1,12 @@
+public class methodoverloading {
+    static void A(){
+        System.out.println(" i m a");
+    }
+    static int A(){
+        return 4;
+    }
+    public static void main(String[] args) {
+        A();
+    }
+    
+}
