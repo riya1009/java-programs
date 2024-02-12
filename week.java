1771eddc6fbbf286c0b48b9ae30d94fb7d11d787
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-12 12:00:04 +0530

    Add files via upload

---
+++ week.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class week {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter choice");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1 :
+            {
+                System.out.println("monday");
+                break;
+            }
+            case 2 :
+            {
+                System.out.println("tuesday");
+                break;
+            }
+            case 3 :
+            {
+                System.out.println("wedday");
+                break;
+            }
+            case 4 :
+            {
+                System.out.println("thursday");
+                break;
+            }
+            case 5 :
+            {
+                System.out.println("friday");
+                break;
+            }
+            case 6 :
+            {
+                System.out.println("saturday");
+                break;
+            }
+            case 7 :
+            {
+                System.out.println("sunday");
+                break;
+            }
+            default:
+            {
+                System.out.println("not a valid option");
+            }
+        }
+    }
+    
+}
