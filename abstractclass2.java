18f3c6f54fd50bb4e387baa301a6dddaeacf0bc6
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-12 11:58:40 +0530

    Add files via upload

---
+++ abstractclass2.java
@@ -0,0 +1,46 @@
+
+
+abstract class Parent2{
+    public Parent2(){
+        System.out.println("Mai parent2 ka constructor hoon");
+    }
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    abstract public void greet();
+    abstract public void greet2();
+}
+
+class Child2 extends Parent2{
+    public Child2(){
+        System.out.println("i m a constructor of child2");
+    }
+    @Override
+    public void greet(){
+        System.out.println("Good morning");
+    }
+    @Override
+    public void greet2(){
+        System.out.println("Good afternoon");
+    }
+}
+
+abstract class Child3 extends Parent2{
+    public void th(){
+        System.out.println("I am good");
+    }
+}
+public class abstractclass2 {
+    public static void main(String[] args) {
+        //Parent2 p = new Parent2(); -- error
+        Child2 c = new Child2();
+        c.greet2();
+        c.greet();
+        c.sayHello();
+        
+        //Child3 c3 = new Child3(); -- error
+    }
+}
+
+    
+
