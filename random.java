4013cf18cf61f9f332a866d38c708eea83e6f249
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-16 20:01:00 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,9 @@
+public class random {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 5;
+        int c = 1;
+        System.out.println(a+ " "+b+c);
+    }
+    
+}
