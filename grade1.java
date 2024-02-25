0fe7ce2f0083b67658cd0542bdb25389e3a01ea4
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-25 19:42:36 +0530

    Add files via upload

---
+++ grade1.java
@@ -0,0 +1,16 @@
+public class grade1 {
+    
+
+
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+        // Decrypting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
+
+    
+
