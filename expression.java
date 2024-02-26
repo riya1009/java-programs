0b2bf4055012c6a9ac6bacdadf039bd56deb9b15
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-26 19:06:47 +0530

    Add files via upload

---
+++ expression.java
@@ -0,0 +1,27 @@
+
+public class expression {
+    public static void main(String[] args) {
+        float x = 9;
+        float y = 8;
+        float v = 6;
+        float u = 4;
+        float a = 3;
+        float b = 2;
+        float c = 5;
+        float d = 1;
+        float exp1 = (x-y)/2;
+        float exp2 = (b*b - 4*a*c)/(2*a);
+        float exp3 = (v*v) - (u*u);
+        float exp4 = a*b-d;
+        System.out.println(exp1);
+        System.out.println(exp2);
+        System.out.println(exp3);
+        System.out.println(exp4);
+        float result1 = 7/4*9/2;
+        System.out.println(result1);
+
+    }
+
+}
+    
+
