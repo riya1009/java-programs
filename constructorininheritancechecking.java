1e89ebed49f4691eac3352dff651b6f647f49502
RIYA BANSAL <srbiiyam@gmail.com>
2024-02-28 19:07:07 +0530

    Add files via upload

---
+++ constructorininheritancechecking.java
@@ -0,0 +1,20 @@
+class Employee{
+   // Employee(){
+     //   System.out.println("hello");
+    //}
+    
+    Employee(int a)
+    {
+        System.out.println(a);
+    }
+}
+class B extends Employee{
+    //B(int b){
+      //  System.out.println(b);
+
+    //}
+
+}
+public class constructorininheritancechecking {
+    
+}
